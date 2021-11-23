c9aee1bb74573d04989352aa5d42ab4b9d5cbf9f
Samwelk <samkamausmart@gmail.com>
2021-11-23 14:53:56 +0300

    Adding tracklisting Component

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -8,7 +8,7 @@ import { Album } from '../album';
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
-  albumInfo:any;
+  albumInfo:Album;
 
   constructor(private _productService: ProductService) { }
 
